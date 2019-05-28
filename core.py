0d1ace2ff0e495e6cd8c3014c3b0bb44554d180d
guiemi <guiemi.teixeira@gmail.com>
2019-05-27 22:41:29 -0300

    Escreve uma função que devolve a quantidade de dias do mês atual

--- core.py
+++ core.py
@@ -1,9 +1,8 @@
 from datetime import date
-
+import calendar
 
 def pick_today():
-    today = date.today().day
-    return today
+    return date.today().day
 
 def actual_month():
     return date.today().month
@@ -12,4 +11,12 @@ def days_left():
     return 31 - pick_today()
 
 
-print(f"Faltam {days_left()} para acabar o mês")
+# print(f"Faltam {days_left()} para acabar o mês")
+
+def last_day():
+    weekday, last_day = calendar.monthrange(date.today().year, actual_month())
+    return last_day
+
+# print(calendar.monthrange(date.today().year, actual_month()))
+
+print(f"Este mês tem {last_day()} dias.")
