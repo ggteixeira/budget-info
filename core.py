3b08b984a03631c58fd0156f0d3889a007838f54
guiemi <guiemi.teixeira@gmail.com>
2019-05-28 20:55:50 -0300

    Mostra quantos dias faltam pro mês acabar e formata o dinheiro
    Essa função já estava pronta

--- core.py
+++ core.py
@@ -7,8 +7,8 @@ def pick_today():
 def actual_month():
     return date.today().month
 
-def days_left():
-    return 31 - pick_today()
+def days_left(last_day):
+    return last_day - pick_today()
 
 
 def last_day():
@@ -20,9 +20,11 @@ def get_money():
     return amount
 
 def calculate_budget(money_amount, present_day, days_left):
-    sugested_budget = (money_amount / days_left)
+    sugested_budget = f"R$ {(money_amount / days_left):.2f}"
     return sugested_budget
 
-print(calculate_budget(get_money(), pick_today(), days_left()))
+print(calculate_budget(get_money(), pick_today(), days_left(last_day())))
+
+print(f"Dias para terminar o mês: {days_left(last_day())}")
 
 
