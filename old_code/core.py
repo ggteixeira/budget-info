cefb12374423c99588cce2ea7061ca38cef2de22
guiemi <guiemi.teixeira@gmail.com>
2020-06-25 21:54:53 -0300

    Adds a method that calculates remaining money and another method that gets how many days are left in the actual month

--- old_code/core.py
+++ old_code/core.py
@@ -17,6 +17,10 @@ class TimeMachine:
 
     def get_last_monthday(self):
         return calendar.monthrange(self.get_today(), self.get_this_month())[1]
+    
+    def get_days_until_last_day(self):
+        return self.get_last_monthday() - self.get_today()
+
 
 
 class Calculator(TimeMachine):
@@ -36,35 +40,19 @@ class Calculator(TimeMachine):
         self.money_amount = money_amount
         print(f"Quantia atualizada de R$ {self.old_amount} para R$ {self.money_amount}")
     
-
+    def calculate_budget(self):
+        # Divide dinheiro pela quantidade de dias restantes
+        return self.money_amount / self.get_days_until_last_day()
 
 
 if __name__ == "__main__":
-    budget = Calculator(500)
-    
-
-# class TimeMachine:
-#     def __init__(self):
-#         self.today = date.today().day
-#         self.this_month = date.today().month
-
-#     def get_last_monthday(self):
-#         return calendar.monthrange(self.today, self.this_month)[1]
-
-#     def get_days_left(self):
-#         return self.get_last_monthday() - self.today
-
-
-# class Calculator(TimeMachine):
-#     def get_money(self):
-#         self.money = float(input("Dinheiro (R$): \n"))
+    budget = Calculator(1000)
+    print(budget)
+    print(budget.get_days_until_last_day())
+    print(budget.calculate_budget())
 
 #     def calculate_budget(self):
 #         return self.money / self.get_days_left()
-
-
-# budget.get_money()
-# budget.get_days_left()
 
 # print(f"Faltam {budget.get_days_left()} dias para o final do mês.", end="")
 # print(f" A sugestão de gasto diária é de R$ {budget.calculate_budget()}")
